931a7fb49bffeb4b8985dc8f14a65bd392bfadb9
Christian Toves <christiano.toves@gmail.com>
2021-08-03 16:40:37 -0700

    draft 1

---
+++ PuzzleJavaTest.java
@@ -0,0 +1,9 @@
+public class PuzzleJavaTest {
+    public static void main (String[] args){
+        PuzzleJava test = new PuzzleJava();
+        test.get10Rolls();
+        test.getNewPasswordSet(10);
+        int[] testNums = {1,7,3,8,9};
+        test.arrayShuffle(testNums);
+    }
+}
